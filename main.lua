16a2bcb15a67cb68ca8e2f38380a4ad16cf33844
Treecher <treechcer@gmail.com>
2025-06-07 22:41:06 +0200

    started working on menu system for lols and giggles, I've had to stop because it's getting late yawn

--- main.lua
+++ main.lua
@@ -3,13 +3,29 @@ player = require("player.player")
 map = require("world.mapGen")
 camera = require("player.camera")
 buildings = require("building.mainBuild")
+game = require("game")
 
-function love.draw()
+local width, height = love.graphics.getDimensions()
 
-    local width, height = love.graphics.getDimensions()
+function drawMenu()
+    button = {
+        height = height / 20,
+        width = width / 3.5,
+        text = "Start Game!"
+    }
 
-    for i = 1, #map, 1 do
-        for j = 1, #map[i], 1 do
+    love.graphics.setColor(1,1,1)
+    love.graphics.rectangle("fill", (width / 2) - (button.width / 2), (height / 2) - (button.height / 2), button.width, button.height)
+
+    love.graphics.setColor(0,0,0)
+    myFont = love.graphics.newFont(24)
+    love.graphics.setFont(myFont)
+    love.graphics.print(button.text, (width / 2) - (button.width / 3), (height / 2) - (button.height / 2))
+end
+
+function drawGame()
+    for i = 1, #map do
+        for j = 1, #map[i] do
             if map[i][j].type == "grass" then
                 love.graphics.setColor(0.29, 0.56, 0.19)
             elseif map[i][j].type == "iron" then
@@ -19,7 +35,8 @@ function love.draw()
             elseif map[i][j].type == "stone" then
                 love.graphics.setColor(0.4, 0.4, 0.45)
             end
-            love.graphics.rectangle("fill", i * width / 10 - (width / 10) - (camera.x), j * height / 10 - (height / 10) - (camera.y), width / 10, height / 10)
+            love.graphics.rectangle("fill", i * width / 10 - width / 10 - camera.x, j * height / 10 - height / 10 - camera.y, width / 10, height / 10
+            )
         end
     end
 
@@ -30,12 +47,23 @@ function love.draw()
     love.graphics.print(player.y, 100, 100)
 end
 
+function love.draw()
+    if game.gameState == "MENU" then
+       drawMenu()
+    elseif game.gameState == "IN-GAME" then
+        drawGame()
+    end
+end
+
 function love.load()
-    
+
 end
 
 function love.update(dt)
-    move = {x = 0, y = 0}
+    if game.gameState == "MENU" then
+
+    elseif game.gameState == "IN-GAME" then
+            move = {x = 0, y = 0}
 
     if love.keyboard.isDown("w")then
         move.y = -1
@@ -60,5 +88,7 @@ function love.update(dt)
         buildings.build()
     end
 
-    camera.viewPort({x = move.x, y = move.y}, dt)
+        camera.viewPort({x = move.x, y = move.y}, dt)
+    end
+
 end
